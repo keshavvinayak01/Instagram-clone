686c5d77bba3c98f3079f8fb5be31bac0c2f1592
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-04 22:25:33 +0530

    Initialized app

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+class MainComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                Reduxstagram
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default MainComponent;
