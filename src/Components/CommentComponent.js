f7af28e3d2d148bf1d82d73b75143b9029188524
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-11 01:51:42 +0530

    Comment Component defined, SingleComponent

---
+++ src/Components/CommentComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CommentComponent extends Component {
+    render() {
+        return (
+            <div className="comment">
+                
+            </div>
+        )
+    }
+}
+
+export default CommentComponent
--- src/Components/SinglePhotoComponent.js
+++ src/Components/SinglePhotoComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-
+import PhotoComponent from './PhotoComponent'
+import CommentComponent from './CommentComponent'
 class SinglePhotoComponent extends Component {
     render() {
+        // fetch index to get the post
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const post = this.props.posts[i];
         return (
             <div>
                 <div class='single-photo'>
-                    I'm the photo
+                    <PhotoComponent i = {i} post={post} {...this.props} />
+                    <CommentComponent />
                 </div>                
             </div>
         )
