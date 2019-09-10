8ec4fde4805f6695e980c6d2d16c70d06f2c8b82
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-11 01:31:04 +0530

    Added UI for individual Picture

--- src/Components/PhotoGridComponent.js
+++ src/Components/PhotoGridComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import PhotoComponent from './PhotoComponent'
 
 class PhotoGridComponent extends Component {
     render() {
         return (
             <div>
                 <div class='photo-grid'>
-                    I'm the photo grid
+                {this.props.posts.map((post, i) => <PhotoComponent {...this.props} key={i} i={i} post={post} /> )}                
                 </div>
             </div>
         )
