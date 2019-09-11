4c70bea15aa3a983418b96ba4ad8a417abe9f718
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-11 11:14:09 +0530

    Added props to comment compoennt

--- src/Components/CommentComponent.js
+++ src/Components/CommentComponent.js
@@ -12,11 +12,17 @@ class CommentComponent extends Component {
             </div>
         )
     }
+    handleSubmit(e){
+        const { postId } = this.props.params;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        this.props.addComment(postId, author, comment);
+    }
     render() {
         return (
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form">
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
                     <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
